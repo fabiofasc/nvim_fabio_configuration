6c58c722952ce6b23410135dc925b563edbfe3c8
fabiofasc <fabiofasc@gmail.com>
2024-10-07 23:06:49 +0200

    configurazione completa nvim by Fabio
    primo commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    tag = "v0.9.1",
+    opts = {
+      ensure_installed = {
+        "javascript",
+        "typescript",
+        "css",
+        "gitignore",
+        "graphql",
+        "http",
+        "json",
+        "scss",
+        "sql",
+        "vim",
+        "lua",
+        "python",
+        "yaml",
+        "dockerfile",
+      },
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+    },
+  },
+}
